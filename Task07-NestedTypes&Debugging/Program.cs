eb1fef8f5575c54d02065e1a843f5710e4d81dc1
Abood03 <abood0alabadi@gmail.com>
2026-08-16 11:02:15 +0300

    Task7 complite

---
+++ Task07-NestedTypes&Debugging/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Company c1 = new Company();
+        Company.Department d1= new Company.Department();
+        d1.Printst(c1);
+        Company c2 = new Company();
+        d1.Printst(c2);
+
+    }
+}
+class Company
+{
+    private string s = "Company class";
+
+    public class Department
+    {
+        public void Printst(Company co)
+        {
+            Console.WriteLine(co.s);
+        }
+    }
+}
