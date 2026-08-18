a5770b47e0d360973b612d315c00e9a4079ed93f
Abood03 <abood0alabadi@gmail.com>
2026-08-18 06:33:23 +0300

    Edit Task2

--- Task02-BooleanLogicArrays/Program.cs
+++ Task02-BooleanLogicArrays/Program.cs
@@ -12,24 +12,51 @@
         {
             Console.Write($"Enter Grade {i+1}:");
             var grade = Console.ReadLine();
-            int.TryParse(grade, out Grade[i]);          
-            if (Grade[i] >= 0) {
-                avg += Grade[i];
-                if (Grade[i] > max)
-                {
-                    max = Grade[i];
-                }
-                if (Grade[i] < min )
-                {
-                    {
-                        min = Grade[i];
-                    }
-
-                }
-                i++;
+            bool isValid = int.TryParse(grade, out int parsedGrade);
+            if (!isValid)
+            {
+                Console.WriteLine("Enter a valid number");
             }
-            else { Console.WriteLine("Enter positive mark "); }
-            
+            else {
+                Grade[i] = parsedGrade;
+                if (Grade[i] >= 0 && Grade[i] <= 100)
+                {
+                    avg += Grade[i];
+                    if (Grade[i] > max)
+                    {
+                        max = Grade[i];
+                    }
+                    if (Grade[i] < min)
+                    {
+                        {
+                            min = Grade[i];
+                        }
+
+                    }
+                    i++;
+                }
+                else { Console.WriteLine("Enter positive mark "); }
+            }
+            if (avg >= 90 && avg <= 100)
+            {
+                Console.WriteLine($"Avg is {avg}: Excellent");
+            }
+            else if (avg >= 80 && avg < 90)
+            {
+                Console.WriteLine($"Avg is {avg}: Very Good");
+            }
+            else if (avg >= 70 && avg < 80)
+            {
+                Console.WriteLine($"Avg is {avg}: Good");
+            }
+            else if (avg >= 60 && avg < 70)
+            {
+                Console.WriteLine($"Avg is {avg}: Fair");
+            }
+            else
+            {
+                Console.WriteLine($"Avg is {avg}: Fail");
+            }
         }
         for (int  i= 0; i < Grade.Length; i++)
         {
