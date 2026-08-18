f92be9baeedfee982f627cb62f684cc606f42e4a
Abood03 <abood0alabadi@gmail.com>
2026-08-18 04:59:44 +0300

    Add simple comments to Task 10 code

--- Task10-GenericsGenericDelegates/Program.cs
+++ Task10-GenericsGenericDelegates/Program.cs
@@ -4,25 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Repository<Employee> repo=new Repository<Employee>();
+        // Create repository and add employees
+        Repository<Employee> repo = new Repository<Employee>();
         repo.Add(new Employee(1, "abood"));
         repo.Add(new Employee(2, "zaid"));
         repo.Add(new Employee(3, "ali"));
-        
-        Console.WriteLine(repo.Find(x => x.Id==1));
-        var filtered= repo.Filter(x => x.Id > 2);
+
+        // Find one employee using Predicate
+        Console.WriteLine(repo.Find(x => x.Id == 1));
+
+        // Filter employees using Func
+        var filtered = repo.Filter(x => x.Id > 2);
         foreach (var item in filtered)
         {
             Console.WriteLine(item);
         }
-         repo.ProcessAll(x => Console.WriteLine(x));
+
+        // Apply an action to every employee
+        repo.ProcessAll(x => Console.WriteLine(x));
 
         Console.WriteLine("------------------------");
-        var getAll=repo.GetAll();
+
+        // Get all employees
+        var getAll = repo.GetAll();
         foreach (var item in getAll)
         {
             Console.WriteLine(item);
         }
+
+        // Handle an ID that does not exist
         try
         {
             Console.WriteLine(repo.GetById(4));
@@ -31,18 +41,24 @@ class Program
         {
             Console.WriteLine(ex.Message);
         }
+
+        // Remove an employee
         Console.WriteLine("Remove");
         repo.Remove(repo.GetById(3));
         repo.ProcessAll(x => Console.WriteLine(x));
-
     }
 }
 
+// Generic repository for any type that implements IEntity
 public class Repository<T> where T : IEntity
 {
+    List<T> item = new List<T>();
+
+    // Return items that match a condition
     public List<T> Filter(Func<T, bool> condition)
     {
         List<T> result = new List<T>();
+
         foreach (var i in item)
         {
             if (condition(i))
@@ -50,17 +66,20 @@ public class Repository<T> where T : IEntity
                 result.Add(i);
             }
         }
-       
 
         return result;
     }
-    public void ProcessAll(Action<T> action) 
+
+    // Run an action on every item
+    public void ProcessAll(Action<T> action)
     {
         foreach (var i in item)
         {
             action(i);
         }
     }
+
+    // Find the first item that matches the predicate
     public T Find(Predicate<T> predicate)
     {
         foreach (var i in item)
@@ -69,41 +88,44 @@ public class Repository<T> where T : IEntity
             {
                 return i;
             }
-            
         }
+
         throw new Exception("Not Found");
     }
-    List <T> item = new List<T>();
 
     public void Add(T entity)
-
     {
         item.Add(entity);
     }
+
     public void Remove(T entity)
     {
         item.Remove(entity);
     }
+
     public List<T> GetAll()
     {
         return item;
     }
-    public T GetById(int id) {
+
+    // Find an item by Id
+    public T GetById(int id)
+    {
         foreach (var i in item)
         {
-
             if (i.Id == id)
                 return i;
         }
+
         throw new Exception("no id founded");
     }
 }
 
-
+// Employee implements IEntity so it can be used in Repository
 public class Employee : IEntity
 {
-    public int Id { get;  set; }
-    public string Name { get;  set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
 
     public Employee(int id, string name)
     {
