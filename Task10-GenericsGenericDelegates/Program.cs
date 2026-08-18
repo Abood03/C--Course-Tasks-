d207f3193419b4129aedb1130c5dc39d72807182
Abood03 <abood0alabadi@gmail.com>
2026-08-18 05:01:36 +0300

    Add simple comments to Task 10 code

--- Task10-GenericsGenericDelegates/Program.cs
+++ Task10-GenericsGenericDelegates/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create repository and add employee
+        // Create repository and add employees
         Repository<Employee> repo = new Repository<Employee>();
         repo.Add(new Employee(1, "abood"));
         repo.Add(new Employee(2, "zaid"));
