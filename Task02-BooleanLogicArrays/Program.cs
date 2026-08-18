070b71ad6e1a93e1ea2d298f24290456418eb4c8
Abood03 <abood0alabadi@gmail.com>
2026-08-18 06:13:23 +0300

    Restore original Task 2 array logic

--- Task02-BooleanLogicArrays/Program.cs
+++ Task02-BooleanLogicArrays/Program.cs
@@ -1,72 +1,73 @@
-﻿using System.Diagnostics;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int x = 3;
-        double d = 4.6;
-        d = x;
-        //x = d; error 
-        x = (int)d;
-        byte b = 200;
-        int z = 300;
+        int max = 0;
+        double avg = 0;
+        int[] Grade = new int [10];
+        int min = int.MaxValue;
+        int temp = 0;
+        Console.WriteLine("Enter your Grade");
+        for (int i = 0; i < Grade.Length; )
+        {
+            Console.Write($"Enter Grade {i+1}:");
+            var grade = Console.ReadLine();
+            int.TryParse( grade, out Grade[i] );
+            avg += Grade[i];
+            if (Grade[i] >= 0) { 
+                if (Grade[i] > max)
+                {
+                    max = Grade[i];
+                }
+                if (Grade[i] < min )
+                {
+                    {
+                        min = Grade[i];
+                    }
+
+                }
+                i++;
+            }
+            else { Console.WriteLine("Enter positive mark "); }
+            
+        }
+        for (int  i= 0; i < Grade.Length; i++)
+        {
+            for (int j = 0; j < Grade.Length-1; j++)
+            {
+                if (Grade[j] > Grade[j + 1])
+                {
+                    temp = Grade[j];
+                    Grade[j] = Grade[j + 1];
+                    Grade[j + 1] = temp;
+                }
+                
+            }
+        }
+        Console.WriteLine("Sorted ");
+        for (int i = 0; i < Grade.Length; i++)
+        {
+            Console.Write($"{Grade[i]},");
+        }
+        for (int i = 0; i < Grade.Length / 2; i++)
+        {
+            temp = Grade[i];
+            Grade[i] = Grade[Grade.Length - 1 - i];
+            Grade[Grade.Length - 1 - i] = temp;
         
-            //b = (byte)z;
+        }
+        Console.WriteLine();
+        Console.WriteLine("Sorted reverse");
         
-        string s = "50";
-
-        Console.WriteLine(int.Parse(s));
-        string st = "hello";
-        try
+        for (int i = 0; i < Grade.Length; i++)
         {
-            int.Parse(st);
-            Console.WriteLine(st);
-
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
+            Console.Write($"{Grade[i]},");
         }
 
-        //Console.WriteLine(Convert.ToInt32("")); //error
-        //Console.WriteLine(Convert.ToInt32("hello")); //error
-
-        string number = "hello";
-        int numm;
-        int.TryParse(number, out numm);
-        Console.WriteLine(numm);
-
-
-
-        //boxing unboxing 
-        int o = 15;
-        Object obj = o;
-        Console.WriteLine($"boxing :{o}");
-        int newnum=(int)o;
-        Console.WriteLine($"unboxing :{newnum}");
-
-
-        Stopwatch sw = Stopwatch.StartNew();
-        sw.Start();
-        for (int i = 0; i < 1000000; i++) 
-        {
-            int sum = 3 + 5;
-        } 
-       sw.Stop();
-        Console.WriteLine($"Normal Loop Time: {sw.ElapsedMilliseconds} ms");
-        sw.Restart();
-        for (int i = 0; i < 1000000; i++) 
-        {
-            int q = 2;
-            Object obj2 = q;
-            int newnum2 = (int)o;
-
-
-        }
-
-        sw.Stop();
-        Console.WriteLine($"Normal Loop Time: {sw.ElapsedMilliseconds} ms");
+        Console.WriteLine();
+        avg = avg / Grade.Length;
+        Console.WriteLine($"Max Value: {max}");
+        Console.WriteLine($"Min Value: {min}");
+        Console.WriteLine($"Avg Value: {avg}");
     }
-
 }
