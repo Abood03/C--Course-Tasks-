5ec903aae2461ad10e0fd9a41e041772e3aac46f
Abood03 <abood0alabadi@gmail.com>
2026-08-28 15:30:09 +0300

    Task 17

---
+++ Task17-NuGetPackagesStringUtilities/NuGetTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using StringUtilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(StringHelper.AddHello("Abood"));
+        Console.WriteLine(StringHelper.IsLong("Hello World From C#"));
+    }
+}
---
+++ Task17-NuGetPackagesStringUtilities/StringUtilities/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace StringUtilities
+{
+    public static class StringHelper
+    {
+        public static string AddHello(string name)
+        {
+            return $"Hello {name}";
+        }
+
+        public static bool IsLong(string text)
+        {
+            return text.Length > 10;
+        }
+    }
+}
