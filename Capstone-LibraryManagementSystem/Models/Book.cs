4b9f8e4c7f4fd289f955f60433808e50cc77bd9c
Abood03 <abood0alabadi@gmail.com>
2026-08-30 13:28:53 +0300

    Start library management system capstone

--- Capstone-LibraryManagementSystem/Models/Book.cs
+++ Capstone-LibraryManagementSystem/Models/Book.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Models
 {
-    internal class Book
+    public class Book
     {
+
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public bool IsBorrowed { get; set; } = false;
+        public Book(int id, string title, string author)
+        {
+            Id = id;
+            Author = author;
+            Title = title;
+        }
+        public override string ToString()
+        {
+            string status = IsBorrowed ? "Borrowed" : "Available";
+
+            return $"Id: {Id}, Title: {Title}, Author: {Author}, Status: {status}";
+        }
+
     }
+
 }
