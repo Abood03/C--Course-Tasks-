994d13f45cad6fa0c9215bf70789d211044eda5b
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:31:24 +0300

    Add XML summary comment

--- Capstone-LibraryManagementSystem/Exceptions/LibraryException.cs
+++ Capstone-LibraryManagementSystem/Exceptions/LibraryException.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Exceptions
 {
+    /// <summary>
+    /// Represents errors raised by library operations.
+    /// </summary>
     public class LibraryException : Exception
     {
         public LibraryException()
