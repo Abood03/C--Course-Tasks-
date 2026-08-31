94af816d5d12de0e9506e87d1ff534b93d6556e1
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:34:15 +0300

    Remove XML summary comment

--- Capstone-LibraryManagementSystem/Models/Librarian.cs
+++ Capstone-LibraryManagementSystem/Models/Librarian.cs
@@ -4,9 +4,6 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Models
 {
-    /// <summary>
-    /// Represents a librarian in the library system.
-    /// </summary>
     public class Librarian : Person
     {
         public Librarian(int id, string name) : base(id, name)
