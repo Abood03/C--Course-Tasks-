c97eccbb3d588da5151f8642e419bf0f28615aca
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:34:22 +0300

    Remove XML summary comment

--- Capstone-LibraryManagementSystem/Models/LibraryService.cs
+++ Capstone-LibraryManagementSystem/Models/LibraryService.cs
@@ -8,9 +8,6 @@ using System.Threading.Tasks;
 
 namespace Capstone_LibraryManagementSystem.Models
 {
-    /// <summary>
-    /// Manages books, members, borrowing, searching, and data persistence.
-    /// </summary>
     public class LibraryService : ISearchable<Book>
     {
         private List<Book> books = new List<Book>();
