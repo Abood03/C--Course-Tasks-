07f8591ddec3a2f3361cf2c3e8fad14e2e45481d
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:34:25 +0300

    Remove XML summary comment

--- Capstone-LibraryManagementSystem/Interfaces/IBorrowable.cs
+++ Capstone-LibraryManagementSystem/Interfaces/IBorrowable.cs
@@ -4,9 +4,6 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Interfaces
 {
-    /// <summary>
-    /// Defines operations for objects that can be borrowed and returned.
-    /// </summary>
     public interface IBorrowable
     {
         public void Borrow();
