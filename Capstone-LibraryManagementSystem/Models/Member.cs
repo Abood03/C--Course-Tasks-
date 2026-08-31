9ce77c4d8f1d5788a3bfb05a7e352df1d3e44a18
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:31:19 +0300

    Add XML summary comment

--- Capstone-LibraryManagementSystem/Models/Member.cs
+++ Capstone-LibraryManagementSystem/Models/Member.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Models
 {
+    /// <summary>
+    /// Represents a library member and tracks borrowed books.
+    /// </summary>
     public class Member : Person
     {
         public List<Book> BorrowedBooks { get; set; } = new List<Book>();
