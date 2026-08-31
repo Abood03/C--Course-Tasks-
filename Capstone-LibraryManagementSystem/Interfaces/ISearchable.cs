183b9bbfcb1b5f66b00e4e4c3ff91360ecbdee6d
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:34:26 +0300

    Remove XML summary comment

--- Capstone-LibraryManagementSystem/Interfaces/ISearchable.cs
+++ Capstone-LibraryManagementSystem/Interfaces/ISearchable.cs
@@ -4,9 +4,6 @@ using System.Text;
 
 namespace Capstone_LibraryManagementSystem.Interfaces
 {
-    /// <summary>
-    /// Defines a generic search operation.
-    /// </summary>
     public interface ISearchable<T>
     {
         List<T> Search(string query);
