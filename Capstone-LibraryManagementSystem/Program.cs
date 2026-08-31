3aebde1d62e8a430e7acf511661ae3f19ae0a261
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:31:25 +0300

    Add XML summary comment

--- Capstone-LibraryManagementSystem/Program.cs
+++ Capstone-LibraryManagementSystem/Program.cs
@@ -4,6 +4,9 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Provides the console application entry point.
+/// </summary>
 class Program
 {
     static async Task Main(string[] args)
