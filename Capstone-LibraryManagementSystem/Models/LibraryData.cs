edb991a419e8545a2fe70eb427566156dd0a16b4
ABDULLAH ALQUAIDER <155391428+Abood03@users.noreply.github.com>
2026-08-31 12:34:14 +0300

    Remove XML summary comment

--- Capstone-LibraryManagementSystem/Models/LibraryData.cs
+++ Capstone-LibraryManagementSystem/Models/LibraryData.cs
@@ -2,9 +2,6 @@
 
 namespace Capstone_LibraryManagementSystem.Models
 {
-    /// <summary>
-    /// Stores books and members for JSON persistence.
-    /// </summary>
     public class LibraryData
     {
         public List<Book> Books { get; set; } = new List<Book>();
